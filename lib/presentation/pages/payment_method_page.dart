8c37f1e138af56d23736255b97bfa095ea848b7c
patic457 <112005137+patic457@users.noreply.github.com>
2022-12-05 13:49:41 +0700

    1349

---
+++ lib/presentation/pages/payment_method_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PaymentMethodPage extends StatefulWidget {
+  const PaymentMethodPage({super.key});
+
+  @override
+  State<PaymentMethodPage> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<PaymentMethodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Text('PaymentMethodPage'));
+  }
+}
