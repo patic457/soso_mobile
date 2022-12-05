e5a40edb8722c7213b5a83fae67e1ff9c491c398
patic457 <112005137+patic457@users.noreply.github.com>
2022-12-05 22:48:52 +0700

    2248

---
+++ lib/presentation/bloc/payment_method_list_event.dart
@@ -0,0 +1,12 @@
+import 'package:equatable/equatable.dart';
+
+abstract class PaymentMethodListEvent extends Equatable {
+  const PaymentMethodListEvent();
+}
+
+class ShowListViewEvent extends PaymentMethodListEvent {
+  const ShowListViewEvent();
+
+  @override
+  List<Object?> get props => throw UnimplementedError();
+}
